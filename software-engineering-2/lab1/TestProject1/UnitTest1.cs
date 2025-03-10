93963bc0be81a2b49c0f8224f83b726d714cd39e
Mateusz Małachowski <01180667@pw.edu.pl>
2025-03-10 13:23:42 +0100

    given-when-then comments

--- software-engineering-2/lab1/TestProject1/UnitTest1.cs
+++ software-engineering-2/lab1/TestProject1/UnitTest1.cs
@@ -8,45 +8,71 @@ namespace TestProject1
         [TestMethod]
         public void EmptyStringTest()
         {
-            int expectedResult = 0;
-            int actualResult = Calculator.Calc("");
 
+            // Given
+            string input = "";
+            int expectedResult = 0;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void SingleNumberTest()
         {
+            // Given
+            string input = "42";
             int expectedResult = 42;
-            int actualResult = Calculator.Calc("42");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void TwoNumbersCommaTest()
         {
+            // Given
+            string input = "10,23";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("10,23");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void TwoNumbersNewlineTest()
         {
+            // Given
+            string input = "10\n23";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("10\n23");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void ThreeNumbersTest()
         {
+            // Given
+            string input = "5\n10,18";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("5\n10,18");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
@@ -54,44 +80,69 @@ namespace TestProject1
         [ExpectedException(typeof(ArgumentException), "Negative numbers not allowed!")]
         public void NegativeNumberTest()
         {
-            Calculator.Calc("-23,10");
+            // Given
+            string input = "-23,10";
 
+            // When
+            Calculator.Calc(input);
+
+            // Then
             Assert.Fail();
         }
 
         [TestMethod]
         public void NumbersGreaterThan1000Test()
         {
+            // Given
+            string input = "10,1001,3333,23";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("10,1001,3333,23");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void CustomSingleDelimiterTest()
         {
+            // Given
+            string input = "//#10#23";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("//#10#23");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void CustomMultiDelimiterTest()
         {
+            // Given
+            string input = "//[#$#]10#$#23";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("//[#$#]10#$#23");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void CustomMultiMultiDelimiterTest()
         {
+            // Given
+            string input = "//[#$#][%%%]5%%%15#$#13";
             int expectedResult = 33;
-            int actualResult = Calculator.Calc("//[#$#][%%%]5%%%15#$#13");
 
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
