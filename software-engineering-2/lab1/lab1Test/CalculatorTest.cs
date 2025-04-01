e0870459365dbd3b84160906c6dccdde2cd73c45
Mateusz Małachowski <01180667@pw.edu.pl>
2025-04-01 17:42:16 +0200

    init branch software-engineering-2

---
+++ software-engineering-2/lab1/lab1Test/CalculatorTest.cs
@@ -0,0 +1,149 @@
+using lab1;
+
+namespace lab1Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+
+            // Given
+            string input = "";
+            int expectedResult = 0;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SingleNumberTest()
+        {
+            // Given
+            string input = "42";
+            int expectedResult = 42;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TwoNumbersCommaTest()
+        {
+            // Given
+            string input = "10,23";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TwoNumbersNewlineTest()
+        {
+            // Given
+            string input = "10\n23";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ThreeNumbersTest()
+        {
+            // Given
+            string input = "5\n10,18";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Negative numbers not allowed!")]
+        public void NegativeNumberTest()
+        {
+            // Given
+            string input = "-23,10";
+
+            // When
+            Calculator.Calc(input);
+
+            // Then
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void NumbersGreaterThan1000Test()
+        {
+            // Given
+            string input = "10,1001,3333,23";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CustomSingleDelimiterTest()
+        {
+            // Given
+            string input = "//#10#23";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CustomMultiDelimiterTest()
+        {
+            // Given
+            string input = "//[#$#]10#$#23";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CustomMultiMultiDelimiterTest()
+        {
+            // Given
+            string input = "//[#$#][%%%]5%%%15#$#13";
+            int expectedResult = 33;
+
+            // When
+            int actualResult = Calculator.Calc(input);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
